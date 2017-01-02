2cc4244119d16c8dbce0ee52bd2c99b4f4175608
Polo Woo <polo@inipro.net>
2017-01-02 18:05:58 +0900

    Update and rename ander3.vhd to ander5.vhd

--- ander5.vhd
+++ ander5.vhd
@@ -1,14 +1,14 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity ander3 is
+entity ander5 is
   port(
     a,b : in std_logic;
     result : out std_logic
   );
-end ander3;
+end ander5;
 
-architecture behavioral of ander3 is
+architecture behavioral of ander5 is
 begin
   process_ex : process(a,b)
     variable temp : std_logic;
