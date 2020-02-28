87e1fbfca8c2f72490554a2f4a2f028a206a8024
Polo Woo <polo@inipro.net>
2020-02-28 18:48:56 +0900

    v2019.2

--- src/top.vhd
+++ src/top.vhd
@@ -1,27 +1,26 @@
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
 entity top is
-    port(
-        top_a,top_b : in std_logic_vector(3 downto 0);
-        top_result : out std_logic_vector(3 downto 0)
+    Port (
+        clk : in std_logic;
+        q : out std_logic_vector(7 downto 0)
     );
 end top;
 
-architecture structural of top is
-    component ander is
-        port(
-            a,b : in std_logic;
-            result : out std_logic
-        );
-    end component;
+architecture Behavioral of top is
+    COMPONENT c_counter_binary_0
+      PORT (
+        CLK : IN STD_LOGIC;
+        Q : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+      );
+    END COMPONENT;
+    signal qtmp : std_logic_vector(31 downto 0);
 begin
-    ander_inst_0 : ander port map (
-        top_a(0), top_b(0), top_result(0));
-    ander_inst_1 : ander port map (
-        top_a(1), top_b(1), top_result(1));
-    ander_inst_2 : ander port map (
-        top_a(2), top_b(2), top_result(2));
-    ander_inst_3 : ander port map (
-        top_a(3), top_b(3), top_result(3));
-end structural;
+    u1 : c_counter_binary_0
+    PORT MAP (
+        CLK => clk,
+        Q => qtmp
+    );
+    q <= qtmp(31 downto 24);    
+end Behavioral;
